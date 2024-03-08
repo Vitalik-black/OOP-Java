f3a53b76b98197333027114e67f0582d60a534c2
Vitalik-black <vitalik.black86@gmail.com>
2024-03-08 16:53:20 +0500

    Доработан крестьянин и Монах.

--- src/units/Monk.java
+++ src/units/Monk.java
@@ -3,18 +3,17 @@ package units;
 import java.util.ArrayList;
 import java.util.*;
 
-// Монах. Своё свойство - лечение
 public class Monk extends Unit {
     int healing;
     int mana;
     boolean flag;
+
     public Monk(String name, int x, int y) {
-        super(name, 150, "null", 0, 4, 10, 50, 40, new Position(x,y));
+        super(name, 150, "null", 0, 4, 10, 50, 40, new Position(x, y));
         healing = 0;
         mana = 10;
         flag = false;
     }
-
 
     @Override
     public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
@@ -29,35 +28,36 @@ public class Monk extends Unit {
                 countdaed++;
             }
         }
-        if (deadlist.size() > 3 ) {
+        if (deadlist.size() > 3) {
             flag = true;
             System.out.println("Флаг установлен");
         }
-
         if (flag && mana == 10) {
-                deadlist.sort((o1, o2) -> o2.speed - o1.speed);
-                deadlist.getFirst().health = maxHealth;
-                mana = 0;
-                System.out.println("Воскресил: " + deadlist.getFirst().name);
-                flag = false;
-                return;
+            deadlist.sort((o1, o2) -> o2.speed - o1.speed);
+            deadlist.getFirst().health = maxHealth;
+            mana = 0;
+            System.out.println("Воскресил: " + deadlist.getFirst().name);
+            flag = false;
+            return;
         }
         if (flag) {
             mana++;
             return;
         }
         if (mana < 2) {
-                mana++;
-                return;
-            }
-            sortlist.getFirst().health += 10;
-            mana -= 2;
+            mana++;
+            return;
         }
-    public String getInfo(){
+        sortlist.getFirst().health += 10;
+        mana -= 2;
+    }
+
+    public String getInfo() {
         return "Монах";
     }
+
     @Override
     public String toString() {
         return super.toString() + ", Мана : " + mana;
     }
-    }
+}
--- src/units/Peasant.java
+++ src/units/Peasant.java
@@ -1,21 +1,18 @@
 package units;
-
 import java.util.ArrayList;
-
-// Крестьянин. Своё свойство - количество стрел
 public class Peasant extends Unit {
     int countArrow;
     protected boolean flag;
+
     public Peasant(String name, int x, int y) {
-        super(name, 150, "arrow", 0, 4, 10, 0, 40, new Position(x,y));
+        super(name, 150, "arrow", 0, 4, 10, 0, 40, new Position(x, y));
         countArrow = 10;
         flag = false;
     }
+
     protected void printname() {
         System.out.println("Name: " + name);
     }
-
-
 
     @Override
     public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
@@ -26,11 +23,13 @@ public class Peasant extends Unit {
     public int getCountArrow() {
         return countArrow;
     }
+
     @Override
     public String toString() {
         return super.toString() + ", \u27b6 : " + countArrow;
     }
-    public String getInfo(){
+
+    public String getInfo() {
         return "Фермер";
-    };
+    }
 }
